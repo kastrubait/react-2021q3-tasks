edd2db0634eaa41c813e5225e7290ae9b40aae5a
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-07-31 02:13:02 +0300

    refactor: global style

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {FaSearch} from "react-icons/fa";
-import {SearchProps} from "../../types/searchProps";
+import {SearchProps} from "../../types/search-props";
 
 import "./style.scss";
 
