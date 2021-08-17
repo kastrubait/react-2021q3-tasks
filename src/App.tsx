ca442dc015bb6f94293ac225933a8857b31c5021
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-08-17 18:07:56 +0300

    feat: animation implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {Route, Switch, useLocation} from "react-router-dom";
+import {Redirect, Route, Switch, useLocation} from "react-router-dom";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 import {About} from "./components/about/About";
 import {Content} from "./components/content/Content";
 import {Header} from "./components/header/Header";
@@ -7,19 +8,21 @@ import {Header} from "./components/header/Header";
 const App: React.FC = () => {
   const location = useLocation();
   return (
-    <>
-      <div className="App">
-        <Header />
-        <Switch location={location}>
-          <Route exact path="/">
-            <Content />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-        </Switch>
-      </div>
-    </>
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="fade" timeout={300}>
+        <div className="App">
+          <Header />
+          <Switch location={location}>
+            <Route exact path="/">
+              <Content />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
 
